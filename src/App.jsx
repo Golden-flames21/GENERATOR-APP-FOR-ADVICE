fc09a6734fcddc89a9e87b602155db98ec0aec24
Golden-flames21 <99491777+Golden-flames21@users.noreply.github.com>
2025-02-17 20:23:18 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+import AdviceApp from './components/AdviceApp';
+
+function App() {
+  return (
+    <div className="main-div">
+      <AdviceApp /> 
+    </div> 
+  );
+}
+
+export default App
---
+++ src/components/AdviceApp.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { useState } from "react";
+import divider from "../assets/pattern-divider-desktop.svg";
+import mobileDivider from "../assets/pattern-divider-mobile.svg"
+import iconDice from "../assets/icon-dice.svg";
+
+const AdviceApp = () => {
+  const [advice, setAdvice] = useState({
+    heading: "66",
+    text: "Take time once in a while to look up at the stars for at least 5 minutes, in order to comprehend your cosmic significance.",
+  });
+  const url = "https://api.adviceslip.com/advice";
+  const fetchAdvice = async () => {
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data);
+    setAdvice({ heading: data.slip.id, text: data.slip.advice });
+  };
+  return (
+    <div className="advice-div">
+      <h5>ADVICE #{advice.heading}</h5>
+      <p>"{advice.text}"</p>
+      <img src={divider} className="divider" />
+      <img src={mobileDivider} className="mobileDivider" />
+
+      <button onClick={fetchAdvice}>
+        <img src={iconDice} />
+      </button>
+    </div>
+  );
+};
+
+export default AdviceApp;
